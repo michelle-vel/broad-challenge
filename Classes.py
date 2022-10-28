1e7170beaa8698c44210c15ab6440485be942fce
michelle-vel <michellevsep@gmail.com>
2022-10-28 03:36:19 -0500

    Add files via upload

---
+++ Classes.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#the name of where you can board or get off an MBTA subway
+# takes in :
+    #name - String
+    #line - String
+    
+class Stop():
+    def __init__(self, _name):
+        self._name = _name 
+        self.id = id
+    
+    def name(self):
+        return self._name
+        
+#route containing multiple stops
+# takes in : 
+    #name - String
+    #stops - list of stop names
+    
+class Route():
+    def __init__(self, _name, _stops):
+        self._name = _name
+        self._stops = _stops
+        self.id = id
+
+
+    def name(self):
+        return self._name
+    
+    def stops(self):
+        return self._stops
+    
+    def __lt__(self, other):
+        return self._name < other._name
+
+    def __eq__(self, other):
+        return self._name == other._name and self._stops == other._stops
+
