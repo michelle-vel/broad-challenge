4308676c3f43abd85f19e89576169d406dcc2dfd
michelle-vel <michellevsep@gmail.com>
2022-10-28 09:32:41 -0500

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -10,15 +10,15 @@ class TestModule(unittest.TestCase):
         all_routes = create_dict(names)
         
     def test_min(self):
-        self.asserEqual(min_max(all_routes, "min"), "Line with fewest stops: Blue , 12 ")
+        self.assertEqual(min_max(all_routes, "min"), "Line with fewest stops: Blue , 12 ")
 
     def test_min(self):
-        self.asserEqual(min_max(all_routes, "max"), "Line with most stops: Red , count: 22")
+        self.assertEqual(min_max(all_routes, "max"), "Line with most stops: Red , count: 22")
     
     def test_algo(self):
-        self.asserEqual(search_algorithm("Ashmont", "Arlington"), "['Red', 'Green-C']")
-        self.asserEqual(search_algorithm("Bowdoin", "Government Center"), "Blue")
-        self.asserEqual(search_algorithm("Sutherland Road", "Cleveland Circle"), "['Green-B', 'Green-C']")
+        self.assertEqual(search_algorithm("Ashmont", "Arlington"), "['Red', 'Green-C']")
+        self.assertEqual(search_algorithm("Bowdoin", "Government Center"), "Blue")
+        self.assertEqual(search_algorithm("Sutherland Road", "Cleveland Circle"), "['Green-B', 'Green-C']")
 
         
 
