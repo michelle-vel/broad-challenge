d9b789891419e8374d7caa67aff8407bc3f9aace
michelle-vel <michellevsep@gmail.com>
2022-10-28 08:24:29 -0500

    Add files via upload

---
+++ Question_2_only.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Return Question 2 Answer
+names = get_id()
+all_routes = create_dict(names)
+
+#get most and least amount of stops for a line
+min_max(all_routes, "min")
+min_max(all_routes, "max")
+
+#get all stops that connect two or more routes
+two_or_more(all_routes)
+
