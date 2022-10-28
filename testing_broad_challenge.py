5ef4dc0cd2c7059b3b4e31c9f67ede755c22c828
michelle-vel <michellevsep@gmail.com>
2022-10-28 08:22:25 -0500

    Add files via upload

---
+++ testing_broad_challenge.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import unittest
+
+
+# In[5]:
+
+
+class TestModule(unittest.TestCase):
+    
+    def set_up():
+        names = get_id()
+        all_routes = create_dict(names)
+        
+    def test_min(self):
+        self.asserEqual(min_max(all_routes, "min"), "Line with fewest stops: Blue , 12 ")
+
+    def test_min(self):
+        self.asserEqual(min_max(all_routes, "max"), "Line with most stops: Red , count: 22")
+    
+    def test_algo(self):
+        self.asserEqual(search_algorithm("Ashmont", "Arlington"), "['Red', 'Green-C']")
+        self.asserEqual(search_algorithm("Bowdoin", "Government Center"), "Blue")
+        self.asserEqual(search_algorithm("Sutherland Road", "Cleveland Circle"), "['Green-B', 'Green-C'])
+
+        
+
+
+# In[ ]:
+
+
+
+
